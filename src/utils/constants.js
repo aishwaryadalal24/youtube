6a9c5acbd6150134ee7b6f30b3721c5f89549699
aishwaryadalal24 <aishwaryadalal24@gmail.com>
2023-11-15 12:59:55 +0530

    Updated youtube suggestions api url to https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const YOU_TUBE_SUGGESSION_API =
-    "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+    "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
